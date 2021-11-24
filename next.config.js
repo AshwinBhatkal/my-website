803575ff8706a7c28d1d7b757fede778ec494243
Ashwin Bhatkal <ashwin96@gmail.com>
2021-11-24 08:55:17 +0530

    Externalised tracking id

--- next.config.js
+++ next.config.js
@@ -3,5 +3,9 @@ module.exports = {
     SERVICE_ID: process.env.SERVICE_ID,
     TEMPLATE_ID: process.env.TEMPLATE_ID,
     USER_ID: process.env.USER_ID,
+    GOOGLE_ANALYTICS: {
+      UA_TRACKING_ID: process.env.UA_TRACKING_ID,
+      GA4_TRACKING_ID: process.env.GA4_TRACKING_ID,
+    },
   },
 };
